4bdae35d6dbe2c39fa611ff6d4cc854d35604368
lheld <lheld@ffdd7af6-1c41-45d9-a44a-668a5760a2da>
2020-12-02 06:01:19 +0000

    bug in pBox fixed
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/replication@139 ffdd7af6-1c41-45d9-a44a-668a5760a2da

--- pkg/R/pBox.R
+++ pkg/R/pBox.R
@@ -1,9 +1,9 @@
-
 zBox <- function(zo, 
                  zr, 
                  c, 
-                 level){
-    z <- qnorm(1 - level/2, lower.tail = TRUE)
+                 level,
+                 alternative=alternative){
+    z <- p2z(level, alternative=alternative)
     den <- ifelse((zo^2 > z^2), c/(zo^2/z^2 - 1) + 1, NA)
     res <- zr^2/den
     return(sqrt(res))
@@ -14,7 +14,7 @@ pBox <- function(zo,
                  c, 
                  level = 0.05,
                  alternative = "two.sided"){
-    z <- zBox(zo = zo, zr = zr, c = c, level = level)
+    z <- zBox(zo = zo, zr = zr, c = c, level = level, alternative = alternative)
     res <- z2p(z)
     if(alternative == "one.sided")
         res <- ifelse(sign(zo) == sign(zr), res/2, 1 - res/2)
